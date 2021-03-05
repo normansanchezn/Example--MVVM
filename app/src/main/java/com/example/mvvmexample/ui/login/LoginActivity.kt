0605c5c3d5c3270849f749cfa0144b575e259046
Norman Sánchez <normansanchezn@gmail.com>
2021-03-05 12:15:46 -0600

    [ADD] Se agrega de forma correcta el MVVM con privacidad en los MutableLiveData

--- app/src/main/java/com/example/mvvmexample/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/mvvmexample/ui/login/LoginActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.databinding.DataBindingUtil
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.mvvmexample.R
 import com.example.mvvmexample.databinding.ActivityMainBinding
 import com.example.mvvmexample.ui.register.RegisterActivity
@@ -11,16 +14,29 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: LoginViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val databinding: ActivityMainBinding = DataBindingUtil.setContentView(this, getLayout())
         configView(databinding)
+
+        viewModel = ViewModelProvider(this)[LoginViewModel::class.java]
+
+        viewModel.progressVisibility.observe(this, Observer { visible ->
+            databinding.progressBar.visibility = if (visible) View.VISIBLE else View.GONE
+        })
+
+        viewModel.message.observe(this, Observer { message ->
+            databinding.tvResult.text = message
+        })
     }
 
     private fun configView(databinding: ActivityMainBinding) {
         with(databinding) {
             btnLogin.setOnClickListener {
-
+                viewModel.onButtonClicked(etUserName.text.toString(), etPassword.text.toString())
             }
 
             tvRegister.setOnClickListener {
--- app/src/main/java/com/example/mvvmexample/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/example/mvvmexample/ui/login/LoginViewModel.kt
@@ -1,6 +1,23 @@
 package com.example.mvvmexample.ui.login
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class LoginViewModel: ViewModel() {
+
+    private val _progressVisibility = MutableLiveData<Boolean>()
+    val progressVisibility: LiveData<Boolean> get() = _progressVisibility
+
+    private val _message = MutableLiveData<String>()
+    val message: LiveData<String> get() = _message
+
+    fun onButtonClicked(user: String, pass: String) {
+        _progressVisibility.value = true
+        _message.value = if (user.isNotEmpty() && pass.isNotEmpty()) "Success" else "Failure"
+        _progressVisibility.value = false
+    }
+
 }
